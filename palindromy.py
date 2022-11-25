eeede58bb6076761c8037f88489e3eef60096583
Maciej Kolakowski <maciej.kolakowski@gmail.com>
2022-11-25 23:05:46 +0100

    Add funkcja .lower()

--- palindromy.py
+++ palindromy.py
@@ -3,7 +3,8 @@ To krótka funkcja do sprawdzenia czy podany wyraz jest palindromem.
 Wynik podawany jest jako True/False 
 """
 def pali_check(word):
-    if str(word)==str(word)[::-1]:
+    if str(word.lower())==str(word.lower())[::-1]:
         print("True")
     else:
         print("False")
+pali_check("kAJaK")
