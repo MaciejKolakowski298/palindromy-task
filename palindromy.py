5adddcecbc50e65dfb70ef0cd9bbbd2c00df2cce
Maciej Kolakowski <maciej.kolakowski@gmail.com>
2022-11-25 22:47:16 +0100

    Add function pali_check

--- palindromy.py
+++ palindromy.py
@@ -2,3 +2,8 @@
 To krótka funkcja do sprawdzenia czy podany wyraz jest palindromem.
 Wynik podawany jest jako True/False 
 """
+def pali_check(word):
+    if str(word)==str(word)[::-1]:
+        print("True")
+    else:
+        print("False")
